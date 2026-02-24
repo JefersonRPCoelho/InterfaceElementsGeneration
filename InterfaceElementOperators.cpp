b283d3448b663ec684cffa2c9c6623bdbee7e148
Jeferson Rômulo Pereira Coêlho <jcoelho@tecgraf.puc-rio.br>
2026-02-24 19:40:38 -0300

    Add dummy class to implement the element operators

---
+++ InterfaceElementOperators.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jcoelho on 2/24/2026.
+//
+
+#include "InterfaceElementOperators.h"
---
+++ InterfaceElementOperators.h
@@ -0,0 +1,16 @@
+//
+// Created by jcoelho on 2/24/2026.
+//
+
+#ifndef 
+#define 
+
+
+
+class InterfaceElementOperators {
+
+};
+
+
+
+#endif //
